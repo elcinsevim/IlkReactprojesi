88a79111b370a9d6dcdf0aba84b5209ef7240ca9
Elçin SEVİM <46248880+elcinsevim@users.noreply.github.com>
2021-01-22 22:58:34 +0300

    ilkCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardExample } from './Examples/CardExample';
+import { Container, Header } from 'semantic-ui-react';
+import { FormExample } from './Examples/FormExample';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Header size='huge'>Huge Header</Header>
+       <CardExample/>
+       <FormExample/>
+    </Container>
+     
+     
   );
 }
 
